5026c2fe06c1933aa7397fc226344a86d7be43e6
Brian Shimkus <brianshimkus1@gmail.com>
2025-05-21 17:40:25 -0500

    Connected to MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,14 @@
 import express from 'express'
 
 import authRoutes from './routes/auth.route.js'
+import { connectDB } from './lib/db.js'
 
 const app = express()
 const PORT = process.env.PORT || 5002
 
 app.use('/api/auth', authRoutes)
 
-const startServer = () => {
-	app.listen(PORT, () => {
-		console.log(`🚀 Server is running on port ${PORT}`)
-	})
-}
-
-startServer()
+app.listen(PORT, () => {
+	console.log(`🚀 Server is running on port ${PORT}`)
+	connectDB()
+})
